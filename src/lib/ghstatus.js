776a91e5556c684c3ebfa83cb2aa1740a3e7eb90
cake-pie <cake-pie@users.noreply.github.com>
2019-03-15 02:20:24 +0800

    test

--- src/lib/ghstatus.js
+++ src/lib/ghstatus.js
@@ -2,7 +2,7 @@ import Octokit from '@octokit/rest'
 
 export const setGithubStatus = errWarnCount => {
   // silently skip unless building on Travis
-  if (!process.env.TRAVIS_JOB_ID) {
+  if (!process.env.TRAVIS) {
     return
   }
 
@@ -12,27 +12,32 @@ export const setGithubStatus = errWarnCount => {
     return
   }
 
-  console.log("Setting Github status for Travis build.");
+  console.log("Setting Github status for Travis build.")
   const octokit = new Octokit({
     auth: 'token ' + process.env.GITHUB_TOKEN
-  });
+  })
   const repoSlug = process.env.TRAVIS_REPO_SLUG.split('/')
   const errors = errWarnCount.errorCount
   const warnings = errWarnCount.warningCount
+  const sha = process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT
+
+  console.log(`${repoSlug[0]}/${repoSlug[1]}/${sha}`)
+  console.log(`https://travis-ci.com/${process.env.TRAVIS_REPO_SLUG}/jobs/${process.env.TRAVIS_JOB_ID}`)
+  console.log(`errors: ${errors} warnings: ${warnings}`)
 
   octokit.repos.createStatus({
     owner: repoSlug[0],
     repo: repoSlug[1],
-    sha: process.env.TRAVIS_COMMIT,
-    context: 'lint-diff',
+    sha: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT,
     state: (errors > 0 ? 'failure' : 'success'),
     target_url: `https://travis-ci.com/${process.env.TRAVIS_REPO_SLUG}/jobs/${process.env.TRAVIS_JOB_ID}`,
-    description: `errors: ${errors} warnings: ${warnings}`
-  }).then(result => {
+    description: `errors: ${errors} warnings: ${warnings}`,
+    context: 'lint-diff'
+  })/**.then(result => {
     if (result.meta.status === '201 Created') {
       console.log("GitHub status created.")
     } else {
       console.log(`Failed to create GitHub status with status ${result.meta.status}`)
     }
-  })
+  })**/
 }
